c9f1a4a97e0a0a68d0dc8c17a0797f4e1524a6c4
dalvarez <anthrax932yt@gmail.com>
2021-02-21 21:19:08 +0100

    Get the output file from the container

--- run.bat
+++ run.bat
@@ -1,14 +1,21 @@
 @echo off
 
-set resources_dir=./src/main/resources
+set resources_dir=src/main/resources
 set input_file_type=excel
 set input_file=%resources_dir%/test.xlsx
 set output_file_type=plain_text
-set output_file=test.csv
+set output_file=%resources_dir%/testDockerfile.csv
 
 docker build --rm ^
     --build-arg INPUT_FILE_TYPE=%input_file_type% ^
     --build-arg INPUT_FILE=%input_file% ^
     --build-arg OUTPUT_FILE_TYPE=%output_file_type% ^
     --build-arg OUTPUT_FILE=%output_file% ^
-    .
+    -t file_parser .
+
+docker create --name temp_file_parser file_parser
+
+set container_base_dir=/app
+
+docker cp temp_file_parser:%container_base_dir%/%output_file% %output_file%
+docker rm temp_file_parser
